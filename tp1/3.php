2a37d1de92958c5aa304334d90dab4bf30b10583
groc.incognito <groc.incognito@gmail.com>
2016-04-06 06:25:16 -0400

    second

---
+++ tp1/3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>hello world en php !</title>
+</head>
+
+<body>
+  <?php
+    $data = array(
+      "Moteurs"=> array(
+        "Google"=> array("url"="https://www.google.fr/","image"="http://www.google.com/logos/doodles/2015/googles-new-logo-5078286822539264.3-hp2x.gif"),
+        "Yahoo"=> array("url"="https://fr.yahoo.com/","image"="https://fr.yahoo.com/")
+      ),
+      "Journaux"=> array(
+        "LeMonde"=> array("url"="http://www.lemonde.fr/","image"="https://pbs.twimg.com/profile_images/671239734904102912/i9lRddT8.jpg"),
+        "LEquipe"=> array("url"="http://www.lequipe.fr/","image"="https://upload.wikimedia.org/wikipedia/fr/3/32/Logo_L%27%C3%89quipe_magazine.jpg")
+      ),
+      "Sports"=> array(
+        "FFF"=> array("url"="https://www.fff.fr/","image"="https://www.fff.fr/common/img/52/68/2/621x423/00552682.jpg"),
+        "FDJ"=> array("url"="https://www.fdj.fr/accueil/","image"="https://upload.wikimedia.org/wikipedia/fr/archive/6/6e/20130423173041!Logo_Fondation_FDJ.svg")
+      )
+    )
+      
+  ?>
+</body>
+
+</html>
